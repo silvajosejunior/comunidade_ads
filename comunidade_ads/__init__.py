52008983986be6f04feac778b56de09690274719
jose junior <josejunioroliveirasilva@gmail.com>
2023-08-06 02:27:47 -0400

    Update __init__.py

--- comunidade_ads/__init__.py
+++ comunidade_ads/__init__.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
+
 app = Flask(__name__)
 
-#lista_Usuarios = ['lira', 'joao', 'maria', 'amanda']
 
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
 if os.getenv("DATABASE_URL"):
@@ -19,5 +20,15 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-success'
 
-from comunidade_ads import routes
+from comunidade_ads import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("base de dados criado")
+else:
+    print("base de dados ja existente")
 
+from comunidade_ads import routes
