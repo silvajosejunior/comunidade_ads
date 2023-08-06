dbc1d33b89b5471bd124879b1d2e226112bd8214
jose junior <josejunioroliveirasilva@gmail.com>
2023-08-06 00:03:58 -0400

    Update __init__.py

--- comunidade_ads/__init__.py
+++ comunidade_ads/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+import os
 app = Flask(__name__)
 
 #lista_Usuarios = ['lira', 'joao', 'maria', 'amanda']
 
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comuna.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comuna.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -17,3 +20,4 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-success'
 
 from comunidade_ads import routes
+
